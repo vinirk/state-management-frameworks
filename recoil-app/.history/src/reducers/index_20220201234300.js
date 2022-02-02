d802e9d7e98524f51d387aaba6a6da0bf5f80cd8
Vinicius Kawamoto <vinirk@gmail.com>
2022-02-02 15:06:21 -0300

    Remove history files

--- recoil-app/.history/src/reducers/index_20220201234300.js
+++
@@ -1,8 +0,0 @@
-import { profile } from "./profile";
-import { home } from "./home";
-import { combineReducers } from "redux";
-
-export default reducers = combineReducers({
-  profile,
-  home,
-});
